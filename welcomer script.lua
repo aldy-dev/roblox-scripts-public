d8ff903e5640790c7aeda12eb1c36b378ecc0323
aldy-dev <theanonymousgamer17@gmail.com>
2024-05-20 13:49:28 +0200

    welcomer script.lua

---
+++ welcomer script.lua
@@ -0,0 +1,9 @@
+-- Script that shows a welcome message to a player when they join
+game.Players.PlayerAdded:Connect(function(player)
+    player:LoadCharacter()
+    local welcomeMessage = Instance.new("Message")
+    welcomeMessage.Text = "Welcome to the game, " .. player.Name .. "!"
+    welcomeMessage.Parent = player.Character
+    wait(5)
+    welcomeMessage:Destroy()
+end)
